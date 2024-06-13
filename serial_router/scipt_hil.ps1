d1a25d7712b73098f4965b791498af98d9ddd096
LorenzoAir22 <lbravi4@gmail.com>
2024-06-13 10:06:00 +0200

    update hill script

--- serial_router/scipt_hil.ps1
+++ serial_router/scipt_hil.ps1
@@ -15,6 +15,8 @@ public class MouseKeyboard
     public const int KEYEVENTF_KEYDOWN = 0x0000;
     public const int KEYEVENTF_KEYUP = 0x0002;
     public const byte VK_NUMLOCK = 0x90;
+    public const byte VK_MENU = 0x12; // Alt key
+    public const byte VK_TAB = 0x09; // Tab key
 
     public static void MoveMouse(int x, int y)
     {
@@ -26,18 +28,48 @@ public class MouseKeyboard
         keybd_event(VK_NUMLOCK, 0, KEYEVENTF_KEYDOWN, 0);
         keybd_event(VK_NUMLOCK, 0, KEYEVENTF_KEYUP, 0);
     }
+
+    public static void PressAltTab()
+    {
+        keybd_event(VK_MENU, 0, KEYEVENTF_KEYDOWN, 0);
+        keybd_event(VK_TAB, 0, KEYEVENTF_KEYDOWN, 0);
+        keybd_event(VK_TAB, 0, KEYEVENTF_KEYUP, 0);
+        keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0);
+    }
 }
 "@
 
-# Generate random coordinates
-$random = [System.Random]::new()
-$screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width
-$screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height
-$randomX = $random.Next(0, $screenWidth)
-$randomY = $random.Next(0, $screenHeight)
+# Infinite loop
+while ($true) {
+    # Generate random coordinates
+    $random = [System.Random]::new()
+    $screenWidth = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width
+    $screenHeight = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height
+    $randomX = $random.Next(0, $screenWidth)
+    $randomY = $random.Next(0, $screenHeight)
 
-# Move the mouse to random coordinates
-[MouseKeyboard]::MoveMouse($randomX, $randomY)
+    # Print the random coordinates
+    Write-Host "Moving mouse to coordinates: X=$randomX, Y=$randomY"
 
-# Press the Num Lock key
-[MouseKeyboard]::PressNumLock()
+    # Move the mouse to random coordinates
+    [MouseKeyboard]::MoveMouse($randomX, $randomY)
+
+    # Print the action of pressing Num Lock
+    Write-Host "Pressing Num Lock key"
+
+    # Press the Num Lock key
+    [MouseKeyboard]::PressNumLock()
+
+    # Print the action of pressing Alt+Tab
+    Write-Host "Pressing Alt+Tab"
+
+    # Press Alt+Tab
+    [MouseKeyboard]::PressAltTab()
+
+    # Generate a random wait time between 1 and 5 seconds
+    $randomWaitTime = $random.Next(1, 6)
+    Write-Host "Waiting for $randomWaitTime seconds"
+
+    # Wait for the random time
+    Start-Sleep -Seconds $randomWaitTime
+}
